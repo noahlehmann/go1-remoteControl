96538a81adee5c5ce18f71309f451b0ecdba254d
noahlehmann <noah.lehmann@protonmail.com>
2023-06-01 14:43:17 +0200

    main: add main features for fe be connection and mqtt logic

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_mqtt import Mqtt
+import struct
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+app.config['MQTT_BROKER_URL'] = "rocky.hof-university.de"
+app.config['MQTT_BROKER_PORT'] = 1883
+mqtt_client = Mqtt(app)
+mqtt_client.publish("controller/action", "walk", qos=2)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+stick_values = {
+    'lx': 0, 'rx': 0, 'ry': 0, 'ly': 0
+}
+preset_speed = 0.6
 
 
 @app.route('/')
@@ -13,6 +24,71 @@ def hello_world():  # put application's code here
 @socketio.on('connect')
 def test_connect():
     print("socket connected")
+
+
+@socketio.on('keydown')
+def set_key(key):
+    print(key + " down")
+    translation = translate_key_to_stick_value(key)
+    stick_values[translation["key"]] = translation["value"]
+    change_stick_values(
+        stick_values["lx"],
+        stick_values["rx"],
+        stick_values["ry"],
+        stick_values["ly"]
+    )
+
+
+@socketio.on('keyup')
+def reset_key(key):
+    print(key + " up")
+    translation = translate_key_to_stick_value(key)
+    stick_values[translation["key"]] = 0
+    change_stick_values(
+        stick_values["lx"],
+        stick_values["rx"],
+        stick_values["ry"],
+        stick_values["ly"]
+    )
+
+
+@socketio.on('color')
+def change_color(info):
+    change_led_color(info.r, info.g, info.b)
+
+
+@mqtt_client.on_log()
+def log_mqtt(msg):
+    print(msg)
+
+
+def change_stick_values(lx, rx, ry, ly):
+    # lx, rx, ry, ly
+    data = struct.pack("ffff", lx, rx, ry, ly)
+    mqtt_client.publish("controller/stick", data, qos=2)
+
+
+def change_led_color(r, g, b):
+    mqtt_client.publish("face_light/color", struct.pack('iii', r, g, b), qos=2)
+
+
+def translate_key_to_stick_value(key):
+    if key == "l_up":
+        return {"key": "ly", "value": preset_speed}
+    if key == "l_down":
+        return {"key": "ly", "value": -preset_speed}
+    if key == "l_left":
+        return {"key": "lx", "value": -preset_speed}
+    if key == "l_right":
+        return {"key": "lx", "value": preset_speed}
+    if key == "r_up":
+        return {"key": "ry", "value": preset_speed}
+    if key == "r_down":
+        return {"key": "ry", "value": -preset_speed}
+    if key == "r_left":
+        return {"key": "rx", "value": -preset_speed}
+    if key == "r_right":
+        return {"key": "rx", "value": preset_speed}
 
 
 if __name__ == '__main__':
--- frontent/src/routes/+page.svelte
+++ frontent/src/routes/+page.svelte
@@ -1,59 +1,109 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import { onMount } from 'svelte';
+    import io from 'socket.io-client';
+    import {writable} from "svelte/store";
+
+    let socketConnected = false;
+    const lastPressed = writable("None");
+    let socket;
+
+    function onKeyDown(e) {
+        lastPressed.set(e.key);
+        switch(e.key) {
+            case "ArrowUp":
+                socket.emit("keydown", "r_up")
+                break;
+            case "ArrowDown":
+                socket.emit("keydown", "r_down")
+                break;
+            case "ArrowLeft":
+                socket.emit("keydown", "r_left")
+                break;
+            case "ArrowRight":
+                socket.emit("keydown", "r_right")
+                break;
+            case "w":
+                socket.emit("keydown", "l_up")
+                break;
+            case "s":
+                socket.emit("keydown", "l_down")
+                break;
+            case "a":
+                socket.emit("keydown", "l_left")
+                break;
+            case "d":
+                socket.emit("keydown", "l_right")
+                break;
+        }
+    }
+
+    function onKeyUp(e) {
+        switch(e.key) {
+            case "ArrowUp":
+                socket.emit("keyup", "r_up")
+                break;
+            case "ArrowDown":
+                socket.emit("keyup", "r_down")
+                break;
+            case "ArrowLeft":
+                socket.emit("keyup", "r_left")
+                break;
+            case "ArrowRight":
+                socket.emit("keyup", "r_right")
+                break;
+            case "w":
+                socket.emit("keyup", "l_up")
+                break;
+            case "s":
+                socket.emit("keyup", "l_down")
+                break;
+            case "a":
+                socket.emit("keyup", "l_left")
+                break;
+            case "d":
+                socket.emit("keyup", "l_right")
+                break;
+        }
+    }
+
+    onMount(() => {
+        socket = io('http://localhost:5000'); // Replace with your server URL
+        socket.on('connect', function() {
+            socketConnected = true;
+            socket.emit('my event', {data: 'I\'m connected!'});
+        });
+
+        return () => {
+            socket.disconnect();
+            socketConnected = false;
+        };
+    });
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app"/>
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+    <p>
+        {#if socketConnected}
+            Connected
+        {:else}
+            Connecting...
+        {/if}
+    </p>
+    <p>{$lastPressed}</p>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+    }
 </style>
+
+<svelte:window on:keydown|preventDefault={onKeyDown} on:keyup|preventDefault={onKeyUp}/>
